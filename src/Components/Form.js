271da4546c88f19a460b7aea8bd4d3616c39a2fd
Nijee Greene <nijeemgreene@gmail.com>
2021-08-27 13:35:36 -0400

    Form validation complete

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,18 +1,63 @@
-import React from 'react'
+import React, {  useRef, useState  } from 'react'
 
 const Form = () => {
-    function handler(){
-        console.log("Working.")
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const twitRef = useRef()
+    const gitRef = useRef()
+    const [error, setError] = useState()
+
+
+    const  handler = (e) =>{
+        e.preventDefault()
+
+        let namere = /^[A-Z][a-z]+ [A-Z][a-z]+[a-z]$/
+        
+        if(!namere.test(nameRef.current.value)){
+            setError("Please enter your full name. First and last. Check for case.")
+            return
+        }
+
+        let emailre= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+        if(!emailre.test(emailRef.current.value)){
+            setError("Invalid Email address")
+            return
+        }
+
+        let twitRe= /^@([A-Z]|[a-z]|[0-9]|_)+$/
+
+        if(!twitRe.test(twitRef.current.value)){
+            setError("Invalid Twitter Handle.")
+            return
+        }
+
+        // Clear text fields
+        Array.from(document.querySelectorAll("input[type=text]")).forEach(
+            input => (input.value = "")
+        )
+        setError("")
+    }
+
+    const eraseData = () =>{
+        Array.from(document.querySelectorAll("input[type=text]")).forEach(
+            input => (input.value = "")
+        )
+        console.log("Data erased!")
+        setError("")
     }
 
     return (
-        <div>
-            <form>
-                <label>Name</label><input type="text" required></input>
-                <label>Email</label><input type="text" required></input>
-                <label>Twitter</label><input type="text"></input>
-                <label>GitHub</label><input type="text"></input>
-                <input onClick= { handler } type="submit"/>
+        <div className = "inputform">
+            <form onSubmit = {  handler  }>
+                <label>Name*</label><input ref= { nameRef } type="text" required></input>
+                <label>Email*</label><input ref = {  emailRef  } type="text" required></input>
+                <label>Twitter</label><input  ref = {  twitRef  } type="text"></input>
+                <label>GitHub</label><input ref = {  gitRef  } type="text"></input>
+                <input type="submit" value="Create"/>
+                <input type="button" onClick = { eraseData } value ="Cancel"></input>
+                <h2>* indicates required field</h2>
+                {error !== "" && <h2 className="errortxt">{error}</h2>}
             </form>
         </div>
     )
